80ddedea06a735ef0cc1aa7ee615385b8b28d874
Spark871 <klm3allemk871champ@gmail.com>
2025-03-10 23:25:22 +0530

    now era has started boy

---
+++ p1.c
@@ -0,0 +1,7 @@
+/*  Flow of the C programming 
+  # High Level language, used to create an os and writing the code of the system softwares
+  # Purpose: Used to write operating systems, compilers, and other system software. 
+  #
+  
+  
+  */
